704bf346caf15da0ffe82072d48040166d0958ab
rafaapcode <rafaap2003@gmail.com>
2024-10-05 16:03:47 -0300

    feat: add the auth header on the interceptors of axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
+import { storageKeys } from "../config/storageKeys";
 
 const client = axios.create({
   baseURL: "http://localhost:3000"
 });
+
+client.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem(storageKeys.accessToken);
+    if(accessToken) {
+      config.headers.set('authorization', `Bearer ${accessToken}`);
+    }
+    return config;
+  },
+)
 
 export default client;
