7d580c268524f3f5818be61fec2959e36114b54f
rafaapcode <rafaap2003@gmail.com>
2024-10-20 12:49:12 -0300

    feat: configuring the time in cache

--- src/components/DataProvider.tsx
+++ src/components/DataProvider.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 type PropsToRenderComponent = {
   data: any;
   isLoading: boolean;
+  isFetching: boolean;
   error: Error | null;
 };
 
@@ -10,12 +11,14 @@ type IDataProviderProps = {
   getData: () => Promise<{status: boolean; data: any}>;
   render: (props: PropsToRenderComponent) => void;
   queryKey: string[];
+  timeInCache?: number;
 }
 
-const DataProvider = ({ getData, render, queryKey }: IDataProviderProps) => {
-  const { data, isFetching, error } = useQuery(
+const DataProvider = ({ getData, render, queryKey, timeInCache }: IDataProviderProps) => {
+  const { data, isFetching, error, isLoading } = useQuery(
     {
       queryKey,
+      staleTime: (timeInCache || 0) * 1000, 
       queryFn: async () => {
         const res = await getData();
         if(res.status) {
@@ -29,7 +32,7 @@ const DataProvider = ({ getData, render, queryKey }: IDataProviderProps) => {
 
   console.log("Dados ",data);
   console.log("Erro ", error);
-  return render({data, error, isLoading: isFetching});
+  return render({data, error, isFetching, isLoading});
 }
 
 export default DataProvider
