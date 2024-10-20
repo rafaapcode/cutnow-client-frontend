6b1e222697fa7d9b0839ac967d1562a3d84ee8e3
rafaapcode <rafaap2003@gmail.com>
2024-10-20 11:32:43 -0300

    feat: change the data provider to use REACT QUERY

--- src/components/DataProvider.tsx
+++ src/components/DataProvider.tsx
@@ -1,27 +1,35 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+type PropsToRenderComponent = {
+  data: any;
+  isLoading: boolean;
+  error: Error | null;
+};
 
 type IDataProviderProps = {
   getData: () => Promise<{status: boolean; data: any}>;
-  render: (props: any) => void;
+  render: (props: PropsToRenderComponent) => void;
+  queryKey: string[];
 }
 
-const DataProvider = ({ getData, render }: IDataProviderProps) => {
-  const [data, setData] = useState(undefined);
-
-  useEffect(() => {
-    (
-      async () => {
+const DataProvider = ({ getData, render, queryKey }: IDataProviderProps) => {
+  const { data, isFetching, error } = useQuery(
+    {
+      queryKey,
+      queryFn: async () => {
         const res = await getData();
         if(res.status) {
-          setData(res.data);
+          return res.data;
         } else {
-          setData(undefined);
+          return undefined;
         }
       }
-    )()
-  }, [getData]);
+    }
+  );
 
-  return render(data);
+  console.log("Dados ",data);
+  console.log("Erro ", error);
+  return render({data, error, isLoading: isFetching});
 }
 
 export default DataProvider
