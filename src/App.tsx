48eccef8e3638e3a857ec5784d523906b876dd35
rafaapcode <rafaap2003@gmail.com>
2024-10-20 08:36:49 -0300

    feat: installing and configuring the queryclient

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./Router";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-   <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   );
 }
 export default App;
