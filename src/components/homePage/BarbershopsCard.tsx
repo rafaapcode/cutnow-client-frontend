9389fe63f4d116d8f452d7d710c4b14fdc38c000
rafaapcode <rafaap2003@gmail.com>
2024-10-26 14:36:28 -0300

    feat: adding default image

--- src/components/homePage/BarbershopsCard.tsx
+++ src/components/homePage/BarbershopsCard.tsx
@@ -26,7 +26,7 @@ const BarbershopsCard = ({ barbershop }: IBarbershopCard) => {
     <div className="w-[90%] md:w-[80%] lg:w-[60%] h-[350px] bg-neutral-900 rounded-md p-3 mx-auto flex flex-col gap-5">
       <div className="w-full h-[230px] overflow-hidden rounded-md">
         <img
-          src={logo}
+          src={logo || "/default-photo.jpg"}
           alt="barbershop logo"
           className="object-cover h-full w-full"
         />
