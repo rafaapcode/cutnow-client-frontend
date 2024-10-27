682eb58a7db9bca759fdf0bede241d8802c96a5d
rafaapcode <rafaap2003@gmail.com>
2024-10-27 13:28:21 -0300

    feat: fetching all schedules of the user

--- src/components/schedules/ScheduleCard.tsx
+++ src/components/schedules/ScheduleCard.tsx
@@ -9,9 +9,15 @@ import {
 
 type IScheduleCard = {
   schedule: {
-    barberImg: string;
-    barberName: string;
-    barbershopName: string;
+    barbeiro: {
+      nome: string;
+      informacoes?: {
+        foto: string;
+      }
+    },
+    barbearia: {
+      nomeDaBarbearia: string;
+    },
     data: string;
   };
 };
@@ -34,18 +40,18 @@ const BarberImg = ({
 };
 
 const ScheduleCard = ({ schedule }: IScheduleCard) => {
-  const { barberImg, barberName, barbershopName, data } = schedule;
-  console.log(barberImg, barberName, barbershopName, data);
+  const { barbearia, barbeiro, data } = schedule;
+  const dataSplit = data.split("T");
   return (
     <div className="w-[90%] md:w-1/2 lg:w-1/4 bg-[#D9D9D9] flex flex-col justify-between rounded-md h-32 p-4 text-black">
-      <h2 className="text-xl font-semibold">Barbearia do Zé</h2>
+      <h2 className="text-xl font-semibold">{barbearia.nomeDaBarbearia}</h2>
       <div className="flex justify-between items-end">
-        <div className="flex gap-3 items-center">
+        <div className="flex gap-3 items-center w-1/2">
           <CiCalendar className="size-4 md:size-6 text-black" />
-          <p className="text-sm">09/10/2024 as 09:00h</p>
+          <p className="text-sm">{dataSplit[0]} as {dataSplit[1]}H</p>
         </div>
-        <BarberImg barberName={barberName}>
-          <UserPhoto className="size-10 shadow-md" avatar={barberImg} />
+        <BarberImg barberName={barbeiro.nome}>
+          <UserPhoto className="size-10 shadow-md" avatar={barbeiro.informacoes?.foto || "./default-photo.jpg"} />
         </BarberImg>
       </div>
     </div>
