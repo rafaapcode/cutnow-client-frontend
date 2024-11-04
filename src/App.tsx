0e5156fe835f8d160639366ae122b0cf278e7f84
rafaapcode <rafaap2003@gmail.com>
2024-11-04 17:12:26 -0300

    feat: remove the queryToo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
 import Router from "./Router";
 
@@ -10,7 +9,6 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Toaster />
       <Router />
-      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
